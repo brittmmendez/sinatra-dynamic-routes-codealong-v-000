2049fb5d6e4fa8380a279e0fb252fbebe607e0c4
Brittany Morris <brittmmendez@gmail.com>
2018-01-20 23:01:18 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,9 +14,9 @@ class App < Sinatra::Base
   end
 
   # Code your final two routes here:
-  get 'goodbye/:name' do
-    @username=params[:name]
-    "Goodbye, #{@user_name}."
+  get "/goodbye/:name" do
+    @user_name = params[:name]
+    "Goodbye #{@user_name}!"
   end
 
 end
